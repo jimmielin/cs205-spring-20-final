5aac51b1bfcb934f2f1c437547bf1145b9b8f8b9
Haipeng Lin <jimmie.lin@gmail.com>
2020-05-05 21:28:34 -0400

    Add a TOC and make an outline

---
+++ cases/memory-light/run_stilt_AWS.r
@@ -0,0 +1,269 @@
+#!/usr/bin/env Rscript
+# STILT R Executable
+# For documentation, see https://uataq.github.io/stilt/
+# Ben Fasoli
+
+# User inputs ------------------------------------------------------------------
+project <- 'stilt_run'
+stilt_wd <- file.path('/shared', project)
+output_wd <- file.path(stilt_wd, 'out')
+lib.loc <- .libPaths()[1]
+
+# Parallel simulation settings
+# n_cores is # of cores per node
+n_cores <- 2
+n_nodes <- 1
+slurm   <- T
+slurm_options <- list(
+  time      = '300:00:00',
+  partition = 'compute'
+)
+
+# Simulation timing, yyyy-mm-dd HH:MM:SS (UTC)
+#t_start <- '2015-06-18 22:00:00'
+#t_end   <- '2015-06-18 22:00:00'
+#run_times <- seq(from = as.POSIXct(t_start, tz = 'UTC'),
+#                 to   = as.POSIXct(t_end, tz = 'UTC'),
+#                 by   = 'hour')
+
+# Receptor location(s)
+lati <- 40.5
+long <- -112.0
+zagl <- 5
+
+# Expand the run times, latitudes, and longitudes to form the unique receptors
+# that are used for each simulation
+
+# receptors <- expand.grid(run_time = run_times, lati = lati, long = long,
+#                        zagl = zagl, KEEP.OUT.ATTRS = F, stringsAsFactors = F)
+
+receptors <- readRDS('/shared/stilt-tutorials/02-train/receptors.rds')
+receptors$run_time <- as.POSIXct('2015-12-10 23:00:00', tz = 'UTC')
+receptors$zagl <- 5
+
+# Footprint grid settings, must set at least xmn, xmx, ymn, ymx below
+hnf_plume <- T
+projection <- '+proj=longlat'
+smooth_factor <- 1
+time_integrate <- F
+xmn <- -112.30
+xmx <- -111.52
+ymn <- 40.390
+ymx <- 40.95
+xres <- 0.002
+yres <- xres
+
+# Meteorological data input
+met_directory   <- '/shared/stilt-tutorials/02-train/met/'
+met_file_format <- '%Y%m%d.%Hz.hrrra'
+n_met_min       <- 1
+
+# Model control
+n_hours    <- -24
+numpar     <- 200
+rm_dat     <- T
+run_foot   <- T
+run_trajec <- T
+timeout    <- 3600
+varsiwant  <- c('time', 'indx', 'long', 'lati', 'zagl', 'sigw', 'tlgr', 'zsfc',
+                'icdx', 'temp', 'samt', 'foot', 'shtf', 'tcld', 'dmas', 'dens',
+                'rhfr', 'sphu', 'solw', 'lcld', 'zloc', 'dswf', 'wout', 'mlht',
+                'rain', 'crai', 'pres')
+
+# Transport and dispersion settings
+conage      <- 48
+cpack       <- 1
+delt        <- 0
+dxf         <- 1
+dyf         <- 1
+dzf         <- 0.1
+emisshrs    <- 0.01
+frhmax      <- 3
+frhs        <- 1
+frme        <- 0.1
+frmr        <- 0
+frts        <- 0.1
+frvs        <- 0.1
+hscale      <- 10800
+ichem       <- 0
+iconvect    <- 0
+initd       <- 0
+isot        <- 0
+kbls        <- 1
+kblt        <- 1
+kdef        <- 1
+khmax       <- 9999
+kmix0       <- 250
+kmixd       <- 3
+kmsl        <- 0
+kpuff       <- 0
+krnd        <- 6
+kspl        <- 1
+kzmix       <- 1
+maxdim      <- 1
+maxpar      <- min(10000, numpar)
+mgmin       <- 2000
+ncycl       <- 0
+ndump       <- 0
+ninit       <- 1
+ndump       <- 0
+ninit       <- 1
+nturb       <- 0
+outdt       <- 0
+outfrac     <- 0.9
+p10f        <- 1
+qcycle      <- 0
+random      <- 1
+splitf      <- 1
+tkerd       <- 0.18
+tkern       <- 0.18
+tlfrac      <- 0.1
+tratio      <- 0.9
+tvmix       <- 1
+veght       <- 0.5
+vscale      <- 200
+w_option    <- 0
+zicontroltf <- 0
+ziscale     <- rep(list(rep(0.8, 24)), nrow(receptors))
+z_top       <- 25000
+
+# Transport error settings
+horcoruverr <- NA
+siguverr    <- NA
+tluverr     <- NA
+zcoruverr   <- NA
+
+horcorzierr <- NA
+sigzierr    <- NA
+tlzierr     <- NA
+
+# Interface to mutate the output object with user defined functions
+before_trajec <- function() {output}
+before_footprint <- function() {output}
+
+
+# Startup messages -------------------------------------------------------------
+message('Initializing STILT')
+message('Number of receptors: ', nrow(receptors))
+message('Number of parallel threads: ', n_nodes * n_cores)
+
+
+# Source dependencies ----------------------------------------------------------
+setwd(stilt_wd)
+source('r/dependencies.r')
+
+
+# Structure out directory ------------------------------------------------------
+# Outputs are organized in three formats. by-id contains simulation files by
+# unique simulation identifier. particles and footprints contain symbolic links
+# to the particle trajectory and footprint files in by-id
+system(paste0('rm -r ', output_wd, '/footprints'), ignore.stderr = T)
+if (run_trajec) {
+  system(paste0('rm -r ', output_wd, '/by-id'), ignore.stderr = T)
+  system(paste0('rm -r ', output_wd, '/particles'), ignore.stderr = T)
+}
+for (d in c('by-id', 'particles', 'footprints')) {
+  d <- file.path(output_wd, d)
+  if (!file.exists(d))
+    dir.create(d, recursive = T)
+}
+
+
+# Met path symlink -------------------------------------------------------------
+# Auto symlink the meteorological data path to the user's home directory to
+# eliminate issues with long (>80 char) paths in fortran
+if ((nchar(paste0(met_directory, met_file_format)) + 2) > 80) {
+  met_loc <- file.path(path.expand('~'), paste0('m', project))
+  if (!file.exists(met_loc)) invisible(file.symlink(met_directory, met_loc))
+} else met_loc <- met_directory
+
+# Run trajectory simulations ---------------------------------------------------
+stilt_apply(FUN = simulation_step,
+            slurm = slurm,
+            slurm_options = slurm_options,
+            n_cores = n_cores,
+            n_nodes = n_nodes,
+            before_footprint = list(before_footprint),
+            before_trajec = list(before_trajec),
+            conage = conage,
+            cpack = cpack,
+            delt = delt,
+            emisshrs = emisshrs,
+            frhmax = frhmax,
+            frhs = frhs,
+            frme = frme,
+            frmr = frmr,
+            frts = frts,
+            frvs = frvs,
+            hnf_plume = hnf_plume,
+            horcoruverr = horcoruverr,
+            horcorzierr = horcorzierr,
+            ichem = ichem,
+            iconvect = iconvect,
+            initd = initd,
+            isot = isot,
+            kbls = kbls,
+            kblt = kblt,
+            kdef = kdef,
+            khmax = khmax,
+            kmix0 = kmix0,
+            kmixd = kmixd,
+            kmsl = kmsl,
+            kpuff = kpuff,
+            krnd = krnd,
+            kspl = kspl,
+            kzmix = kzmix,
+            maxdim = maxdim,
+            maxpar = maxpar,
+            lib.loc = lib.loc,
+            met_file_format = met_file_format,
+            met_loc = met_loc,
+            mgmin = mgmin,
+            n_hours = n_hours,
+            n_met_min = n_met_min,
+            ncycl = ncycl,
+            ndump = ndump,
+            ninit = ninit,
+            nturb = nturb,
+            numpar = numpar,
+            outdt = outdt,
+            outfrac = outfrac,
+            output_wd = output_wd,
+            p10f = p10f,
+            projection = projection,
+            qcycle = qcycle,
+            r_run_time = receptors$run_time,
+            r_lati = receptors$lati,
+            r_long = receptors$long,
+            r_zagl = receptors$zagl,
+            random = random,
+            rm_dat = rm_dat,
+            run_foot = run_foot,
+            run_trajec = run_trajec,
+            siguverr = siguverr,
+            sigzierr = sigzierr,
+            smooth_factor = smooth_factor,
+            splitf = splitf,
+            stilt_wd = stilt_wd,
+            time_integrate = time_integrate,
+            timeout = timeout,
+            tkerd = tkerd, tkern = tkern,
+            tlfrac = tlfrac,
+            tluverr = tluverr,
+            tlzierr = tlzierr,
+            tratio = tratio,
+            tvmix = tvmix,
+            varsiwant = list(varsiwant),
+            veght = veght,
+            vscale = vscale,
+            w_option = w_option,
+            xmn = xmn,
+            xmx = xmx,
+            xres = xres,
+            ymn = ymn,
+            ymx = ymx,
+            yres = yres,
+            zicontroltf = zicontroltf,
+            ziscale = ziscale,
+            z_top = z_top,
+            zcoruverr = zcoruverr)
--- docs/index.md
+++ docs/index.md
@@ -1,28 +1,75 @@
 # CS205 Spring 2020 Final Project: STILT Parallelization 
 Quick intro or sth 
 
+- [TOC - CS205 Spring 2020 Final Project: STILT Parallelization](#cs205-spring-2020-final-project--stilt-parallelization)
+  * [Abstract](#abstract)
+  * [Introduction](#introduction)
+    + [Problem Description](#problem-description)
+    + [Model Code](#model-code)
+    + [The Needs for HPC and Big Data](#the-needs-for-hpc-and-big-data)
+  * [Test Cases Experiments](#test-cases-experiments)
+  * [Parallel Architecture Design](#parallel-architecture-design)
+    + [Software Architecture](#software-architecture)
+    + [Parallel Methods and Performance Analysis](#parallel-methods-and-performance-analysis)
+      - [SLURM-based node parallelization](#slurm-based-node-parallelization)
+      - [AWS Batch-based parallelization](#aws-batch-based-parallelization)
+    + [Reproducibility Information](#reproducibility-information)
+      - [SLURM-based on Harvard Cannon](#slurm-based-on-harvard-cannon)
+      - [SLURM-based on AWS Cloud](#slurm-based-on-aws-cloud)
+      - [AWS Batch](#aws-batch)
+  * [Conclusion](#conclusion)
+  * [References](#references)
+
 ## Abstract
 ...
 
 ## Introduction
-### Problems
-Stochastic Time-Inverted Lagrangian Transport Model or STILT model is an atmospheric model that simulates air parcel movements using ensembles of particles starting from a particular time and location.  Although the model can run both forward and backward in time from the given starting location, the backward runs are more commonly used. The knowledge of air particle trajectories depends on the specified receptors, which are the locations that the measurements were made.  From the information obtained via STILT, one can construct the influence of an atmospheric observation, which has proven to be
-extremely useful for understanding atmospheric datasets. As the numbers of particles and receptors increase, the information of specific atmospheric datasets increases as well as the computational cost. Thus, the main challenge of using STILT is to come up with optimal numbers of receptors, numbers of particles, and the time that needs to be running backward for.
+### Problem Description
+Stochastic Time-Inverted Lagrangian Transport Model or STILT model is an atmospheric model that simulates air parcel movements using ensembles of particles starting from a particular time and location.  Although the model can run both forward and backward in time from the given starting location, the backward runs are more commonly used. The knowledge of air particle trajectories depends on the specified receptors, which are the locations that the measurements were made.  From the information obtained via STILT, one can construct the influence of an atmospheric observation, which has proven to be extremely useful for understanding atmospheric datasets. As the numbers of particles and receptors increase, the information of specific atmospheric datasets increases as well as the computational cost. Thus, the main challenge of using STILT is to come up with optimal numbers of receptors, numbers of particles, and the time that needs to be running backward for.
 
+### Model Code
+The STILT model code used in this work is available at ... 
 
 ### The Needs for HPC and Big Data
-    
-## Experiments
 
-In a STILT run, one needs to provide at least (i) an emission dataset, (ii) a meteorological dataset, (iii) a receptor dataset, and (iv) an R script. 
+## Test Cases Experiments
+
+We use two cases with different computational profiles to investigate STILT parallelization. A memory-lightweight case serves as proof-of-concept and is intended to be easily parallelizable. A second case is memory-intensive and ...
+
+A STILT run requires the following datasets to run: (i) an emission dataset, (ii) a meteorological dataset, (iii) a receptor dataset, and (iv) an R script. 
 
 For the memory-light case, we used what has been provided on the official STILT site.
 https://uataq.github.io/stilt/tutorials/train.html
 
-For the memory-intensive case, we used emission and receptor datasets based on a research project that studies greenhouse gas emissions in the New York City area. The meteorological product that we chose was the Global Forecast System. The meteorological reanalysis was from March 1st, 2019 https://www.ncdc.noaa.gov/data-access/model-data/model-datasets/global-forcast-system-gfs
-	
+For the memory-intensive case, we used emission and receptor datasets based on a research project that studies greenhouse gas emissions in the New York City area. The meteorological product that we chose was the Global Forecast System. The meteorological reanalysis was from March 1st, 2019 https://www.ncdc.noaa.gov/data-access/model-data/model-datasets/global-forcast-system-gfs.
 
-
-## Summary 
+## Parallel Architecture Design
 
 ...
+
+### Software Architecture
+
+### Parallel Methods and Performance Analysis
+#### SLURM-based node parallelization
+
+STILT includes a SLURM workload manager-based parallelization capability which we benchmark, analyze and optimized on the two computational cases previously described. ...
+
+#### AWS Batch-based parallelization
+
+...
+
+### Reproducibility Information
+#### SLURM-based on Harvard Cannon
+
+#### SLURM-based on AWS Cloud
+
+#### AWS Batch
+
+...
+
+## Conclusion 
+
+...
+
+
+## References
